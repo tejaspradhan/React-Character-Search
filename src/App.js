40083904725622f728a778aff2ea702ded9ccd3f
Tejas Pradhan <pradhan.tejas135@gmail.com>
2021-03-13 15:28:29 +0530

    modified CardList component

--- src/App.js
+++ src/App.js
@@ -5,15 +5,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      characters: [
-        // { name: "Leonard", id: "1" },
-        // { name: "Sheldon", id: "2" },
-        // { name: "Howard", id: "3" },
-        // { name: "Raj", id: "4" },
-        // { name: "Bernadette", id: "5" },
-        // { name: "Amy", id: "6" },
-        // { name: "Penny", id: "7" },
-      ],
+      characters: [],
     };
   }
   changeText = () => {
@@ -28,11 +20,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Character Search Engine</h1>
-        <CardList name="Tejas">
-          {this.state.characters.map((chars) => (
-            <h1 key={chars.id}>{chars.name}</h1>
-          ))}
-        </CardList>
+        <CardList characters={this.state.characters} />
       </div>
     );
   }
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import "./card-list.styles.css";
 export const CardList = (props) => {
-  return <div className="card-list">{props.children}</div>;
+  return (
+    <div className="card-list">
+      {props.characters.map((chars) => (
+        <h1 key={chars.id}>{chars.name}</h1>
+      ))}
+    </div>
+  );
 };
